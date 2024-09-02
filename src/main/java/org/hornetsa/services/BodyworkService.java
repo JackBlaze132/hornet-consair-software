28997d00ee66efa84eec52b8922adad5538a4d57
Jhon Cardenas <jhoncardenas712@gmail.com>
2024-09-02 15:44:34 -0500

    New BodyworkService Class

---
+++ src/main/java/org/hornetsa/services/BodyworkService.java
@@ -0,0 +1,47 @@
+package org.hornetsa.services;
+
+import org.hornetsa.model.Automobile;
+import org.hornetsa.model.Bodywork;
+import org.hornetsa.model.Vehicle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BodyworkService {
+
+    private ArrayList<Bodywork> bodyworks;
+
+    public BodyworkService() {
+        bodyworks = new ArrayList<>();
+    }
+
+    public BodyworkService(ArrayList<Bodywork> bodyworks) {
+        this.bodyworks = bodyworks;
+    }
+
+    public void addBodywork(Bodywork bodywork) {
+        bodyworks.add(bodywork);
+    }
+
+    public ArrayList<Bodywork> getBodyworks() {
+        return bodyworks;
+    }
+
+    public Bodywork getBodywork(int idBody) {
+        for (Bodywork bodywork : bodyworks) {
+            if (bodywork.getIdBody() == idBody) {
+                return bodywork;
+            }
+        }
+        return null;
+    }
+
+    public void removeBodywork(int idBody) {
+        Bodywork bodywork = getBodywork(idBody);
+        if (bodywork != null) {
+            bodyworks.remove(bodywork);
+        }
+    }
+
+
+}
